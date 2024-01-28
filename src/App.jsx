0950a3f2cc48c420c9507c6f83e1a8b240de6862
FrankySuazo <frankysuazo@outlook.com>
2024-01-28 09:14:27 -0500

    submition2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { userState } from "react";
 
 //Error on line 4 due to 'color' and 'setFunction' for missing props. Need feed back on how to fix it -Franky
-
+//update
 const Color = ({ color, setFunction }) => {
   const [selected, setSelected] = userState("");
   return (
