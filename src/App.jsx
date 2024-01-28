805020c00be6eb4d27883eba4205d655ae248c93
FrankySuazo <frankysuazo@outlook.com>
2024-01-28 09:10:13 -0500

    submition

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 // Write your Color component here
+import { userState } from "react";
+
+//Error on line 4 due to 'color' and 'setFunction' for missing props. Need feed back on how to fix it -Franky
+
+const Color = ({ color, setFunction }) => {
+  const [selected, setSelected] = userState("");
+  return (
+    <div
+      className={`${color} ${selected}`}
+      onClick={() => {
+        setSelected(selected === "selected" ? "" : "selected");
+        setFunction(color);
+      }}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = userState("blue");
   return (
     <div id="container">
       <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div> Currently Selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        {
+          <>
+            <Color color="blue" setFunction={setSelectedColor} />
+            <Color color="red" setFunction={setSelectedColor} />
+            <Color color="violet" setFunction={setSelectedColor} />
+          </>
+        }
+      </div>
     </div>
   );
 };
